3a2185427717747072ed486d92217b5e7c861d0e
J1lson <gelsonyurisimao@gmail.com>
2023-06-24 16:12:03 +0000

    Primeira atualizacao do Jilson

--- ProjetoEscola/src/projetoescola/ProjetoEscola.java
+++ ProjetoEscola/src/projetoescola/ProjetoEscola.java
@@ -16,6 +16,7 @@ public class ProjetoEscola {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Meu primeiro Envio!");
     }
     
 }
